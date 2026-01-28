c70338d5044e7843e25bc378b0af42450f3a5ab7
Zaza4real <sass839@gmail.com>
2026-01-28 09:04:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ const app = express();
 /* ---------------------------
    CORS (demo-friendly)
 ---------------------------- */
+const allowedOrigins = new Set([
+  "https://digitalgeekworld.com",
+  "https://www.digitalgeekworld.com",
+  "https://homepage-3d78.onrender.com"
+]);
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Stripe-Signature");
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.has(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
