52cc057effd729c67107e330c48c8a7fd080ca96
Zaza4real <sass839@gmail.com>
2026-01-29 20:57:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
   }
   res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
 });
